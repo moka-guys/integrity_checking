5c3f49877d3987825b482b556b118dd8ddd10cd8
aledj2 <aledjones@nhs.net>
2019-06-06 11:27:35 +0100

    fix #7 automate testing. add automated script flags to ignore file to stop run progressing without affecting integrity test

--- integrity_checking_config.py
+++ integrity_checking_config.py
@@ -4,11 +4,11 @@ integrity_checking configuration script.
 Variables defined in this module are required by sequencer_checksum.py.
 """
 # Set debug mode
-#debug = True
-debug = False
+debug = True
+#debug = False
 
 # =====git release for the github repo=====
-script_release = "v22.0"
+script_release = "v23.0"
 
 # ================ Integrity check
 # the filename which holds the checksum results
@@ -26,5 +26,7 @@ max_number_of_attempts = 10
 # list of files which differ between temp and output
 missing_files_output = "missing_files.txt"
 # files to exclude from integrity check
-exclude = ["RTAStart.bat", "CorrectedIntMetrics.bin", "EmpiricalPhasingMetrics.bin", "ErrorMetrics.bin", "EventMetrics.bin", "ExtractionMetrics.bin", "PFGridMetrics.bin", "QMetrics.bin", "RegistrationMetrics.bin", "TileMetrics.bin", "AlignmentMetrics.bin", "BasecallingMetrics.bin", "ExtendedTileMetrics.bin", "FWHMGridMetrics.bin", "ImageMetrics.bin", "QGridMetrics.bin", "000_000_000_na_rtabat.trans", "FilesAdded.csv", "FilesCopied.csv", "md5checksum.txt", missing_files_output]
+exclude = ["RTAStart.bat", "CorrectedIntMetrics.bin", "EmpiricalPhasingMetrics.bin", "ErrorMetrics.bin", "EventMetrics.bin", "ExtractionMetrics.bin", "PFGridMetrics.bin", "QMetrics.bin", "RegistrationMetrics.bin", "TileMetrics.bin", "AlignmentMetrics.bin", "BasecallingMetrics.bin", "ExtendedTileMetrics.bin", "FWHMGridMetrics.bin", "ImageMetrics.bin", "QGridMetrics.bin", "000_000_000_na_rtabat.trans", "FilesAdded.csv", "FilesCopied.csv", "demultiplexlog.txt", "DNANexus_upload_started.txt", "md5checksum.txt", missing_files_output]
+# files to move as part of automated testing
+files_to_move = ["RunInfo.xml"]
 
